12a12cbbd164b179848c031dcd4418ee3ae47d68
otatsuya <tao754@ensign.edu>
2024-07-16 13:03:02 -0600

    Lab10b

--- src/main/java/BlackJack/BJHand.java
+++ src/main/java/BlackJack/BJHand.java
@@ -1,4 +1,84 @@
 package BlackJack;
 
-public class BJHand {
+import java.util.ArrayList;
+
+import BlackJackBase.PCard;
+import BlackJackBase.PHand;
+
+public class BJHand implements PHand{
+
+    private ArrayList<BJCard> hands;
+
+    public BJHand() {
+        hands = new ArrayList<BJCard>();
+    }
+
+    @Override
+    public int getSize() {
+        return hands.size();
+    }
+
+    @Override
+    public void addCard(PCard card) {
+        if(card == null) {
+
+        }else {
+            hands.add((BJCard)card);
+        }
+    }
+
+    @Override
+    public PCard getCard(int index) {
+        BJCard card = hands.get(index);
+        return (PCard)card;
+    }
+
+    @Override
+    public PCard removeCard(int index) {
+        if (index >= 0 && index < hands.size()) {
+            return hands.remove(index);
+        }else {
+            return null;
+        }
+
+    }
+
+    @Override
+    public int getValue() {
+        int totalNum = 0;
+        int totalAce = 0;
+        for (BJCard c: hands) {
+            int num =0;
+            switch(c.getRank()) {
+                case 1:
+                    totalAce += 1;
+                    num = 11;
+                    break;
+                case 11:
+                    num = 10;
+                    break;
+                case 12:
+                    num = 10;
+                    break;
+                case 13:
+                    num = 10;
+                    break;
+                case 14:
+                    num = 10;
+                    break;
+                default:
+                    num = c.getRank();
+                    break;
+            }
+            totalNum += num;
+        }
+
+        for(int i = 0 ; i < totalAce ; i++) {
+            if(totalNum > 21 ) {
+                totalNum -= 10;
+            }
+        }
+        return totalNum;
+    }
+
 }
--- src/main/java/BlackJackTest/PlayBlackJack.java
+++ src/main/java/BlackJackTest/PlayBlackJack.java
@@ -1,48 +1,49 @@
 package BlackJackTest;
 
-
-import BlackJack.BJCard;
 import BlackJack.BJDeck;
-import BlackJackBase.PCard;
-import BlackJackBase.PDeck;
+import BlackJack.BJHand;
 
 public class PlayBlackJack {
 
     public static void main(String[] args) {
+        // TODO Auto-generated method stub
 
-        PDeck deck = new BJDeck();
+//  BJCard card0 = new BJCard(BJCard.ACE,BJCard.SPADE);
+//  BJCard card1 = new BJCard(BJCard.SIX,BJCard.HEART);
+//  BJCard card2= new BJCard(BJCard.JACK,BJCard.SPADE);
+//  BJCard card3 = new BJCard(BJCard.KNIGHT,BJCard.HEART);
+//  BJCard card4 = new BJCard(BJCard.QUEEN,BJCard.DIAMOND);
+//  BJCard card5 = new BJCard(BJCard.KING,BJCard.CLUB);
+//
+//
+//        // Pass the PCard object to the run() method
+//        CardTest.run(card0);
+//        CardTest.run(card1);
+//        CardTest.run(card2);
+//        CardTest.run(card3);
+//        CardTest.run(card4);
+//        CardTest.run(card5);
 
-        DeckTest.run(deck);
+        BJDeck deck = new BJDeck();
+        BJHand hand = new BJHand();
+//  DeckTest.run(deck);
+        HandTest.run(deck, hand);
+
+//  PGame.setBackgroundColor(Color.GREEN);
+//  PGame.setBannerColor(Color.DARK_GRAY);
+//  PGame.setBannerTextColor(Color.white);
+//  PGame.setStatusTextColor(Color.BLACK);
+//  PGame.setButtonColor(Color.LIGHT_GRAY);
+//  PGame.setButtonTextColor(Color.BLACK);
+//  PGame.setButtonHighlightColor(Color.orange);
+//  PGame.setFont("Helvetica");
+//  BJDeck deck = new BJDeck();
+//  BJHand dealer = new BJHand();
+//  BJHand player = new BJHand();
+//  PGame.run(deck, player, dealer);
+//
+    }
 
 
-/*
-        PCard aceSpade = new BJCard(BJCard.ACE, BJCard.SPADE);
-
-        CardTest.run(aceSpade);
-
-        PCard sixHeart = new BJCard(BJCard.SIX, BJCard.HEART);
-
-        CardTest.run(sixHeart);
-
-        PCard jackSpade = new BJCard(BJCard.JACK, BJCard.SPADE);
-
-        CardTest.run(jackSpade);
-
-        PCard knightHeart = new BJCard(BJCard.KNIGHT, BJCard.HEART);
-
-        CardTest.run(knightHeart);
-
-        PCard queenDiamond = new BJCard(BJCard.QUEEN, BJCard.DIAMOND);
-
-        CardTest.run(queenDiamond);
-
-        PCard kingClub = new BJCard(BJCard.KING, BJCard.CLUB);
-
-        CardTest.run(kingClub);
-        */
-
-
-    }//end of main
-
-}//end of PlayBlackJack
+}
 
